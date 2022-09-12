491059c0b9f0538436724f73d7037eeb5dceee53
cbolles <collinbolles@gmail.com>
2022-09-12 10:01:21 -0400

    Remove no-longer needed code

--- server/src/services/jwt.strategy.ts
+++ server/src/services/jwt.strategy.ts
@@ -1,11 +1,14 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
-import {ConfigService} from '@nestjs/config';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { UserService } from './user.service';
+import { TokenPayload } from 'shared/dtos/auth.dto';
+import { User } from '../schemas/user.schema';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
+  constructor(configService: ConfigService, private userService: UserService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -13,7 +16,16 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
-    return payload;
+  /**
+   * Check for the existance of the user with the given ID and return that
+   * user
+   */
+  async validate(payload: TokenPayload): Promise<User> {
+    const user = await this.userService.find(payload._id);
+    if (!user) {
+      throw new UnauthorizedException(`User with id ${payload._id} does not exist`);
+    }
+
+    return user;
   }
 }
