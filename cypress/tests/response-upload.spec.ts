7df74c6406d5a73db8e77b616673a7c9d59e1565
cbolles <collinbolles@gmail.com>
2022-09-13 14:17:39 -0400

    Begin work on testing video upload functionality

--- cypress/tests/response-upload.spec.ts
+++ cypress/tests/response-upload.spec.ts
@@ -1,12 +1,11 @@
-describe('Response Upload', () => {
+const uploadResponsesButton = '[data-cy="uploadResponsesButton"]';
+const uploadCSVButton = '[data-cy="uploadCSVButton"]';
+const uploadZIPButton = '[data-cy="uploadZIPButton"]';
+const csvFileUploadInput = '[data-cy="csvFileUploadInput"]';
+const uploadStatusMessage = '[data-cy="uploadStatusMessage"]';
+const zipFileUploadInput = '[data-cy="zipFileUploadInput"]';
 
-  const uploadResponsesButton = '[data-cy="uploadResponsesButton"]';
-  const uploadCSVButton = '[data-cy="uploadCSVButton"]';
-  const uploadZIPButton = '[data-cy="uploadZIPButton"]';
-  const csvFileUploadInput = '[data-cy="csvFileUploadInput"]';
-  const uploadStatusMessage = '[data-cy="uploadStatusMessage"]';
-
-
+describe('Upload CSV', () => {
   beforeEach(() => {
     // Clear out any existing data
     cy.resetDB();
@@ -95,5 +94,45 @@ describe('Response Upload', () => {
       .get(uploadZIPButton)
       .should('not.be.disabled');
   });
+});
 
+describe('Upload Response ZIP', () => {
+  beforeEach(() => {
+    // Clear out any existing data
+    cy.resetDB();
+    cy.firstTimeSetup();
+
+    // Navigate to the response interface and select the upload option
+    // then upload the small dataset
+    cy
+      .login()
+      .visit('/admin')
+      .get('div[class="mat-tab-label-content"]')
+      .contains('Responses')
+      .click()
+      .wait(100)
+      .get(uploadResponsesButton)
+      .click()
+      .get(csvFileUploadInput)
+      .selectFile('cypress/fixtures/responses/small-set.csv', { force: true })
+  });
+
+  // TODO: Uncomment once the bug associating with uploading empty zip
+  //       files has been addressed
+  /*
+  it('should warn when nothing in zip folder', () => {
+    cy
+      .get(zipFileUploadInput)
+      .selectFile('cypress/fixtures/responses/empty.zip', { force: true })
+      .get('li');
+  });
+  */
+
+  it('should produce a warning when not all of the videos have been provided in a zip', () => {
+    cy
+      .get(zipFileUploadInput)
+      .selectFile('cypress/fixtures/responses/small-set-missing.zip', { force: true })
+      .get('li')
+      .should('contain.text', 'hi');
+  });
 });
