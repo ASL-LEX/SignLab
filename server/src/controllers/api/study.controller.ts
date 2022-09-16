ae94f85e77d45d4f927a09c061435441b97490c4
cbolles <collinbolles@gmail.com>
2022-09-16 10:31:53 -0400

    Remove guard around access to the studies

--- server/src/controllers/api/study.controller.ts
+++ server/src/controllers/api/study.controller.ts
@@ -31,7 +31,6 @@ export class StudyController {
    * Get all of the studies
    */
   @Get('/')
-  @Auth('admin')
   async getStudies(): Promise<Study[]> {
     return this.studyService.getStudies();
   }
