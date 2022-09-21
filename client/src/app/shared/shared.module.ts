bbac1036cdd7b96f5a2061039a1546b6b9adf6f1
cbolles <collinbolles@gmail.com>
2022-09-21 16:06:54 -0400

    Extract generic code from asl-lex-field to video-option-field

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,20 +1,21 @@
 // Components
 import { AslLexSignBankField } from './components/custom-fields/asl-lex-field.component';
 import { FileListField } from './components/custom-fields/file-list.component';
+import { VideoOptionField } from './components/custom-fields/video-option-field.component';
 
 // Modules
 import { NgModule } from '@angular/core';
 import { JsonFormsModule } from '@jsonforms/angular';
 import { JsonFormsAngularMaterialModule } from '@jsonforms/angular-material';
 import { FlexModule } from '@angular/flex-layout/flex';
-import { MaterialModule } from '../material.module'; // TODO: Have this in shared
+import { MaterialModule } from '../material.module';
 import { FormsModule } from '@angular/forms';
 
 // Pipes
 import { SafePipe } from './pipes/safe.pipe';
 
 @NgModule({
-  declarations: [AslLexSignBankField, FileListField, SafePipe],
+  declarations: [AslLexSignBankField, FileListField, SafePipe, VideoOptionField],
   imports: [
     FormsModule,
     MaterialModule,
