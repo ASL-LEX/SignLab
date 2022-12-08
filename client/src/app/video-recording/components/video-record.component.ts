1f701e48f41804432236f2cc89a4d963927f1ce8
cbolles <collinbolles@gmail.com>
2022-12-08 14:02:38 -0500

    Make wrapper for the video view and recording logic

--- client/src/app/video-recording/components/video-record.component.ts
+++ client/src/app/video-recording/components/video-record.component.ts
@@ -1,12 +1,11 @@
 import {
   ChangeDetectorRef,
   Component,
-  ElementRef,
   ViewChild,
   EventEmitter,
   Output,
-  OnDestroy,
 } from '@angular/core';
+import { VideoPreviewComponent } from './video-preview.component';
 
 /**
  * Interface for recording a single video. The video will be stored and
@@ -14,12 +13,30 @@ import {
  */
 @Component({
   selector: 'video-record',
-  templateUrl: './video-record.component.html',
+  template:
+    `
+    <div fxLayout="column" fxLayoutAlign="center center" class="videoContainer">
+      <!-- Recording info message -->
+      <div fxLayout="row" fxLayoutAlign="start center" class="recording-info">
+        <mat-icon *ngIf="isRecording" class="recordingIndicator">videocam</mat-icon>
+        <span>{{ isRecording ? "Recording..." : "Preview" }}</span>
+      </div>
+
+      <video-preview #videoPreview (video)="videoBlob.emit($event)"></video-preview>
+
+      <!-- Button to start/stop recording -->
+      <div>
+        <button (click)="toggleRecording()" mat-stroked-button>
+          {{ isRecording ? 'Stop Recording' : 'Start Recording' }}
+        </button>
+      </div>
+    </div>
+  `,
   styleUrls: ['./video-record.component.css'],
 })
-export class VideoRecordComponent implements OnDestroy {
+export class VideoRecordComponent {
   /** The view element for the video element */
-  @ViewChild('recordVideo') recordVideo: ElementRef;
+  @ViewChild('videoPreview') recordVideo: VideoPreviewComponent;
   /** Keeps track of if the user is actively recording a video. */
   isRecording = false;
   /** The video stream from the user's webcam. */
@@ -33,10 +50,10 @@ export class VideoRecordComponent implements OnDestroy {
 
   toggleRecording(): void {
     if (this.isRecording) {
-      this.stopRecording();
+      this.recordVideo.stopRecording();
       this.isRecording = false;
     } else {
-      this.startRecording().then((isSuccess) => {
+      this.recordVideo.startRecording().then((isSuccess: boolean) => {
         this.isRecording = isSuccess;
 
         // Force change detection to update view. The view was not detecting
@@ -44,101 +61,5 @@ export class VideoRecordComponent implements OnDestroy {
         this.changeDetector.detectChanges();
       });
     }
-  }
-
-  /** Start the recording, return true if successful */
-  private async startRecording(): Promise<boolean> {
-    // Make a new stream, clear out original data
-    this.recordVideo.nativeElement.currentTime = 0;
-    let stream: MediaStream;
-    try {
-      stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-      });
-    } catch (err: any) {
-      console.debug('Could not get user media', err);
-      this.isRecording = false;
-
-      // Let the user know that they need to enable their webcam.
-      const message =
-        'Unable to access webcam, make sure you have given permission' +
-        'to access your webcam and that no other application is using it.';
-      this.displayWebcamError(message);
-      return false;
-    }
-
-    this.recordVideo.nativeElement.srcObject = stream;
-    this.blobs = [];
-
-    let options: MediaRecorderOptions = { mimeType: 'video/webm' };
-    if (MediaRecorder.isTypeSupported('video/webm;codecs=vp9')) {
-      options = { mimeType: 'video/webm; codecs=vp9' };
-    } else if (MediaRecorder.isTypeSupported('video/webm;codecs=vp8')) {
-      options = { mimeType: 'video/webm; codecs=vp8' };
-    } else {
-      console.error('Cannot instantiate mediaRecorder');
-      this.displayWebcamError(
-        'Unable to record video, please try again and ' +
-          'report this issue if it persists.'
-      );
-      return false;
-    }
-
-    // Start the recording
-    this.mediaRecorder = new MediaRecorder(stream, options);
-    this.mediaRecorder.start();
-    this.recordVideo.nativeElement.play();
-
-    // Set up event listeners
-    this.mediaRecorder.ondataavailable = (event) => {
-      this.onBlobAvailable(event);
-    };
-    this.mediaRecorder.onstop = (_event) => {
-      this.onMediaStop();
-    };
-
-    // Return success
-    return true;
-  }
-
-  stopRecording(): void {
-    this.mediaRecorder.stop();
-    this.recordVideo.nativeElement.pause();
-  }
-
-  ngOnDestroy(): void {
-    // Clear out the video element
-    this.recordVideo.nativeElement.pause();
-    this.recordVideo.nativeElement.src = '';
-    this.recordVideo.nativeElement.load();
-    this.recordVideo.nativeElement.remove();
-  }
-
-  /** Let the user know that they need to enable their webcam. */
-  displayWebcamError(message: string): void {
-    console.debug(message);
-    alert(message);
-  }
-
-  /**
-   * Handles the logic of storing produced blobs into an array
-   */
-  private onBlobAvailable(event: BlobEvent): void {
-    this.blobs.push(event.data);
-  }
-
-  /**
-   * Handles the logic of collecting the blobs, producing the
-   * cooresponding video, and updating the video element to show the
-   * user the recorded video.
-   */
-  private onMediaStop(): void {
-    const videoBuffer = new Blob(this.blobs, { type: 'video/webm' });
-    const videoUrl = URL.createObjectURL(videoBuffer);
-    this.recordVideo.nativeElement.srcObject = null;
-    this.recordVideo.nativeElement.src = videoUrl;
-
-    // Emit the completed video
-    this.videoBlob.emit(videoBuffer);
   }
 }
--- client/src/app/video-recording/video-recording.module.ts
+++ client/src/app/video-recording/video-recording.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { VideoFieldComponent } from './components/video-field.component';
 import { VideoRecordComponent } from './components/video-record.component';
+import { VideoPreviewComponent } from './components/video-preview.component';
 import { CoreModule } from '../core/core.module';
 
 @NgModule({
-  declarations: [VideoRecordComponent, VideoFieldComponent],
+  declarations: [VideoRecordComponent, VideoFieldComponent, VideoPreviewComponent],
   imports: [SharedModule, CoreModule],
   exports: [VideoRecordComponent, VideoFieldComponent],
   providers: [],
