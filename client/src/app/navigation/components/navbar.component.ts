909c60a760837f0b05224406a460d6e54406a76d
cbolles <collinbolles@gmail.com>
2023-01-04 16:10:42 -0500

    Remove code duplication around navigation elements

--- client/src/app/navigation/components/navbar.component.ts
+++ client/src/app/navigation/components/navbar.component.ts
@@ -1,11 +1,86 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../core/services/auth.service';
 
+
+interface NavItem {
+  name: string;
+  url: string;
+  sublinks?: NavItem[];
+}
+
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  navItems: NavItem[] = [
+    {
+      name: 'Organization',
+      url: '/organization',
+      sublinks: [
+        {
+          name: 'User Permissions',
+          url: '/organization/user-permissions'
+        },
+      ],
+    },
+    {
+      name: 'Project',
+      url: '/project',
+      sublinks: [
+        {
+          name: 'User Permissions',
+          url: '/project/user-permissions'
+        },
+        {
+          name: 'Dataset Access',
+          url: '/project/dataset-access'
+        },
+        {
+          name: 'Create New Project',
+          url: '/project/create-new-project'
+        }
+      ],
+    },
+    {
+      name: 'Studies',
+      url: '/studies',
+      sublinks: [
+        {
+          name: 'User Permissions',
+          url: '/studies/user-permissions'
+        },
+        {
+          name: 'Entry Controls',
+          url: '/studies/entry-controls'
+        },
+        {
+          name: 'Contribute',
+          url: '/studies/contribute'
+        },
+        {
+          name: 'Create New Study',
+          url: '/studies/create-new-study'
+        }
+      ],
+    },
+    {
+      name: 'Datasets',
+      url: '/datasets',
+      sublinks: [
+        {
+          name: 'Upload Entries',
+          url: '/datasets/upload-entries'
+        },
+        {
+          name: 'Create New Dataset',
+          url: '/datasets/create-new-dataset'
+        }
+      ],
+    },
+  ];
+
+
   constructor(public authService: AuthService) {}
 }
--- client/src/app/navigation/navigation.module.ts
+++ client/src/app/navigation/navigation.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { NavbarComponent } from './components/navbar.component';
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [SharedModule],
+  imports: [SharedModule, RouterModule],
   exports: [NavbarComponent]
 })
 export class NavigationModule { }
