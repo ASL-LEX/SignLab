1ea7ae723ba6ff58ddd6bcdcf8926cca752c6869
cbolles <collinbolles@gmail.com>
2023-01-06 10:49:10 -0500

    Have study guard open dialog for selecting study

--- client/src/app/core/guards/study.guard.ts
+++ client/src/app/core/guards/study.guard.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import {
   CanActivate,
   Router,
@@ -6,6 +7,8 @@ import {
   RouterStateSnapshot,
 } from '@angular/router';
 import { StudyService } from '../services/study.service';
+import { StudySelectDialog } from '../../admin-dashboard/components/studies-control/study-select-dialog.component';
+import { Observable, of } from 'rxjs';
 
 /**
  * Check to see if an active study is selected, if not, redirect to the study
@@ -13,14 +16,27 @@ import { StudyService } from '../services/study.service';
  */
 @Injectable()
 export class StudyGuard implements CanActivate {
-  constructor(private studyService: StudyService, private router: Router) {}
+  constructor(private studyService: StudyService, private router: Router, private dialog: MatDialog) {}
 
-  canActivate(_route: ActivatedRouteSnapshot, _state: RouterStateSnapshot) {
+  canActivate(_route: ActivatedRouteSnapshot, _state: RouterStateSnapshot): Observable<boolean> {
     if (this.studyService.getActiveStudy()) {
-      return true;
+      return of(true);
     }
 
-    this.router.navigate(['/studies/select/']);
-    return false;
+    return this.openDialog();
+  }
+
+  private openDialog(): Observable<boolean> {
+    const dialogRef = this.dialog.open(StudySelectDialog, {
+      width: '500px',
+      data: {},
+    });
+
+    return dialogRef.afterClosed().pipe((selectedStudy: any) => {
+      if (selectedStudy) {
+        return of(true);
+      }
+      return of(false);
+    });
   }
 }
