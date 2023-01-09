406de02c03f44d26112ef554c08fbdf688d4e11e
cbolles <collinbolles@gmail.com>
2023-01-09 12:03:08 -0500

    Wrap project select in card

--- client/src/app/navigation/components/study-select.component.ts
+++ client/src/app/navigation/components/study-select.component.ts
@@ -3,14 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'study-select',
   template: `
-    <div fxLayout="row" fxLayoutAlign="center" class="study-select">
-      <p>Study: </p>
-      <mat-select class="select-field">
-        <mat-option value="1">Study 1</mat-option>
-        <mat-option value="2">Study 2</mat-option>
-        <mat-option value="3">Study 3</mat-option>
-      </mat-select>
-    </div>
+    <mat-card class="study-select-card">
+      <mat-card-content>
+        <div fxLayout="row" fxLayoutAlign="center" class="study-select">
+          <p>Study: </p>
+          <mat-select class="select-field">
+            <mat-option value="1">Study 1</mat-option>
+            <mat-option value="2">Study 2</mat-option>
+            <mat-option value="3">Study 3</mat-option>
+          </mat-select>
+        </div>
+      </mat-card-content>
+    </mat-card>
   `,
   styleUrls: ['./study-select.component.css']
 })
