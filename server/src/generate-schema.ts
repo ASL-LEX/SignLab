dfecb6d509f46ef23ee9298d58ed6ae2ef12ebc7
cbolles <collinbolles@gmail.com>
2023-01-19 13:15:57 -0500

    Add the ability to generate the GraphQL schema from the CLI

---
+++ server/src/generate-schema.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { GraphQLSchemaBuilderModule, GraphQLSchemaFactory } from '@nestjs/graphql';
+import { writeFileSync } from 'fs';
+import { printSchema } from 'graphql';
+import { join } from 'path';
+import {ProjectResolver} from './project/project.resolver';
+
+const resolvers = [
+  ProjectResolver,
+];
+
+const main = async () => {
+  const app = await NestFactory.create(GraphQLSchemaBuilderModule);
+  await app.init();
+
+  const gqlSchemaFactory = app.get(GraphQLSchemaFactory);
+  const schema = await gqlSchemaFactory.create(resolvers, []);
+
+  writeFileSync(join(process.cwd(), '/dist/schema.gql'), printSchema(schema));
+};
+main();
