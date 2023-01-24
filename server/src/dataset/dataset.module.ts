820ddb9ac19d4a644ae7fb1989ae49e4595ccdec
cbolles <collinbolles@gmail.com>
2023-01-24 16:35:40 -0500

    Remove dataset controller

--- server/src/dataset/dataset.module.ts
+++ server/src/dataset/dataset.module.ts
@@ -2,7 +2,6 @@ import { Module, forwardRef } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import {SharedModule} from 'src/shared/shared.module';
 import { AuthModule } from '../auth/auth.module';
-import { DatasetController } from './dataset.controller';
 import {DatasetResolver} from './dataset.resolver';
 import { Dataset, DatasetSchema } from './dataset.schema';
 import { DatasetService } from './dataset.service';
@@ -13,7 +12,6 @@ import { DatasetService } from './dataset.service';
     forwardRef(() => AuthModule),
     forwardRef(() => SharedModule),
   ],
-  controllers: [DatasetController],
   providers: [DatasetService, DatasetResolver],
   exports: [DatasetService],
 })
