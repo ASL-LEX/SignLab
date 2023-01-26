2fd1788beba8b60a4c204121eade03d79bb27411
cbolles <collinbolles@gmail.com>
2023-01-26 12:33:45 -0500

    Add in project tables

--- client/src/app/datasets/components/project-access.component.ts
+++ client/src/app/datasets/components/project-access.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DatasetService } from '../../core/services/dataset.service';
+import { ProjectService } from '../../core/services/project.service';
 
 /**
  * View which determins which projects have the ability to access the
@@ -7,7 +9,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'project-access',
   template: `
-    <p>Well hi there!</p>
+    <div *ngIf="datasetService.datasets | async as datasets">
+      <mat-expansion-panel *ngFor="let dataset of datasets">
+        <mat-expansion-panel-header>
+          <mat-panel-title> {{ dataset.name }} </mat-panel-title>
+          <mat-panel-description> {{ dataset.description }} </mat-panel-description>
+        </mat-expansion-panel-header>
+
+        <!-- Project View Table -->
+        <div *ngIf="projectService.projects | async as projects" class="project-grid">
+          <table mat-table [dataSource]="projects" class="mat-elevation-z8">
+            <ng-container matColumnDef="projectName">
+              <th mat-header-cell *matHeaderCellDef> Project Name </th>
+              <td mat-cell *matCellDef="let project"> {{ project.name }} </td>
+            </ng-container>
+
+            <ng-container matColumnDef="projectDescription">
+              <th mat-header-cell *matHeaderCellDef> Project Description </th>
+              <td mat-cell *matCellDef="let project"> {{ project.description }} </td>
+            </ng-container>
+
+            <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+            <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
+          </table>
+        </div>
+
+      </mat-expansion-panel>
+    </div>
   `,
+  styleUrls: ['./project-access.component.css'],
 })
-export class ProjectAccess {}
+export class ProjectAccess {
+  displayedColumns: string[] = ['projectName', 'projectDescription'];
+
+  constructor(public readonly datasetService: DatasetService, public readonly projectService: ProjectService) {}
+}
