7f31d512529c8aa3f28504a2abdcc6384a372db5
cbolles <collinbolles@gmail.com>
2023-01-27 12:18:46 -0500

    Have the new study view see limited datasets

--- client/src/app/dataset-table/components/dataset-new-study.component.ts
+++ client/src/app/dataset-table/components/dataset-new-study.component.ts
@@ -7,7 +7,7 @@ import { DatasetService } from '../../core/services/dataset.service';
 @Component({
   selector: 'dataset-new-study-table',
   template: `
-    <div *ngIf="datasetService.datasets | async as datasets">
+    <div *ngIf="datasetService.visibleDatasets | async as datasets">
       <mat-expansion-panel *ngFor="let dataset of datasets">
         <mat-expansion-panel-header>
           <mat-panel-title>{{ dataset.name }}</mat-panel-title>
