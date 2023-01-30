ff0b47d2a2ea01c3ba7eb4e0f68b88f15057a9c3
cbolles <collinbolles@gmail.com>
2023-01-30 14:43:33 -0500

    Begin re-adding in the user training download button

--- client/src/app/studies/components/user-permissions.component.ts
+++ client/src/app/studies/components/user-permissions.component.ts
@@ -16,7 +16,7 @@ export class UserPermissionsComponent {
   activeStudyID: string | null = null;
   /** The currently selected project ID */
   activeProjectID: string | null = null;
-  displayedColumns = ['name', 'username', 'email', 'studyAdmin', 'contribute'];
+  displayedColumns = ['name', 'username', 'email', 'studyAdmin', 'contribute', 'taggingTrainingResults'];
 
   constructor(
     public studyService: StudyService,
@@ -58,5 +58,60 @@ export class UserPermissionsComponent {
     }
 
     toggleChange.change.source.checked = toggleChange.user.roles.studyContributor[this.activeStudyID!];
+  }
+
+    /*!*
+   * Download the training tags as a CSV
+   *
+   * NOTE: This is a tempory feature for exporting information and
+   *       will be changed in future versions
+   */
+  async downloadUserTraining(user: User) {
+    if (!this.activeStudyID) {
+      return;
+    }
+
+    const tags = await this.studyService.getTrainingTags(user, this.activeStudyID);
+
+    const flattenedData = tags.map((tag) => {
+      return {
+        entryID: tag.entry.entryID,
+        mediaURL: tag.entry.mediaURL,
+        study: tag.study.name,
+        user: tag.user.username,
+        ...tag.entry.meta,
+        ...tag.info,
+      };
+    });
+
+    if (flattenedData.length == 0) {
+      alert('No training data');
+      return;
+    }
+
+    this.downloadFile(user, flattenedData);
+  }
+
+  private downloadFile(user: User, data: any[]) {
+    const replacer = (_key: string, value: any) =>
+      value === null ? '' : value; // specify how you want to handle null values here
+    const header = Object.keys(data[0]);
+    const csv = data.map((row) =>
+      header
+        .map((fieldName) => JSON.stringify(row[fieldName], replacer))
+        .join(',')
+    );
+    csv.unshift(header.join(','));
+    const csvArray = csv.join('\r\n');
+
+    const a = document.createElement('a');
+    const blob = new Blob([csvArray], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+
+    a.href = url;
+    a.download = `${user.username}-training.csv`;
+    a.click();
+    window.URL.revokeObjectURL(url);
+    a.remove();
   }
 }
