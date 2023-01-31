968b508601aeefe57015fab6401d4b622c7c2997
cbolles <collinbolles@gmail.com>
2023-01-31 15:51:23 -0500

    Add authorization header to graphql requests

--- client/src/app/graphql.module.ts
+++ client/src/app/graphql.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
-import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { ApolloClientOptions, InMemoryCache, ApolloLink } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
+import { setContext } from '@apollo/client/link/context';
 
+// TODO: Bring in URL from configuration
 const uri = 'http://localhost:3000/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  const auth = setContext((_operation, _context) => {
+    const authInfo = localStorage.getItem('SIGNLAB_AUTH_INFO');
+    if (authInfo === null) {
+      return {};
+    }
+    const { token } = JSON.parse(authInfo);
+    return {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    }
+  });
+
   return {
-    link: httpLink.create({ uri }),
+    link: ApolloLink.from([auth, httpLink.create({ uri })]),
     cache: new InMemoryCache()
   };
 }
