e29a6a2a8e9b235f7540602274e552a15d48296c
cbolles <collinbolles@gmail.com>
2023-02-02 15:57:07 -0500

    Add the ability to redirect on auth failure

--- client/src/app/graphql.module.ts
+++ client/src/app/graphql.module.ts
@@ -3,10 +3,14 @@ import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { ApolloClientOptions, InMemoryCache, ApolloLink } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
 import { setContext } from '@apollo/client/link/context';
+import { Router, RouterModule } from '@angular/router';
+import { onError } from '@apollo/client/link/error'
 
 // TODO: Bring in URL from configuration
 const uri = 'http://localhost:3000/graphql'; // <-- add the URL of the GraphQL server here
-export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+export function createApollo(httpLink: HttpLink, router: Router): ApolloClientOptions<any> {
+
+  // Logic to get the token from local storage
   const auth = setContext((_operation, _context) => {
     const authInfo = localStorage.getItem('SIGNLAB_AUTH_INFO');
     if (authInfo === null) {
@@ -20,19 +24,31 @@ export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
     }
   });
 
+  // Logic to handle auth errors
+  const errorLink = onError(({ graphQLErrors }) => {
+    if (graphQLErrors) {
+      for (const error of graphQLErrors) {
+        if (error.extensions.code === 'UNAUTHENTICATED') {
+          router.navigate(['/auth']);
+        }
+      }
+    }
+  });
+
   return {
-    link: ApolloLink.from([auth, httpLink.create({ uri })]),
+    link: ApolloLink.from([auth, errorLink, httpLink.create({ uri })]),
     cache: new InMemoryCache()
   };
 }
 
 @NgModule({
+  imports: [RouterModule],
   exports: [ApolloModule],
   providers: [
     {
       provide: APOLLO_OPTIONS,
       useFactory: createApollo,
-      deps: [HttpLink]
+      deps: [HttpLink, Router],
     }
   ]
 })
