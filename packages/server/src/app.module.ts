fedb8b16029df8c2167c3e12e07e0e71fdd1d3c4
cbolles <collinbolles@gmail.com>
2023-05-17 11:17:03 -0400

    Move environment files into server package

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -36,7 +36,7 @@ let configModule = ConfigModule.forRoot({
 // If a specific environment is provided, load variables from there
 if (process.env.NODE_ENV) {
   configModule = ConfigModule.forRoot({
-    envFilePath: `../.env.${process.env.NODE_ENV}`,
+    envFilePath: `.env.${process.env.NODE_ENV}`,
     load: [configuration]
   });
 }
