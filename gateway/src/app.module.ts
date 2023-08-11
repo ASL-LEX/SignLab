58c2f2b3e3ea1e1dd9e1abf1d987bbb064e682d0
cbolles <collinbolles@gmail.com>
2023-08-11 14:38:20 -0400

    Add gateway

---
+++ gateway/src/app.module.ts
@@ -0,0 +1,45 @@
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration]
+    }),
+    GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      driver: ApolloGatewayDriver,
+      useFactory: async (configService: ConfigService) => ({
+        autoSchemaFile: {
+          path: 'schema.gql',
+          federation: 2
+        },
+        gateway: {
+          buildService: ({ url }) => {
+            return new RemoteGraphQLDataSource({
+              url,
+              willSendRequest({ request, context }) {
+                if (context.req && context.req.headers) {
+                  // Copy over authentication
+                  request.http!.headers.set('authorization', context.req.headers.authorization);
+                }
+              }
+            });
+          },
+          supergraphSdl: new IntrospectAndCompose({
+            subgraphs: [
+              { name: 'signlab', url: configService.getOrThrow('signlab.uri') },
+              { name: 'lex_service', url: configService.getOrThrow('lex_service.uri') },
+            ]
+          })
+        }
+      })
+    })
+  ]
+})
+export class AppModule {}
---
+++ gateway/src/config/configuration.ts
@@ -0,0 +1,8 @@
+export default () => ({
+  signlab: {
+    uri: process.env.SIGNLAB_URI
+  },
+  lex_service: {
+    uri: process.env.LEX_URI
+  }
+});
